68e21a2974e1849dbb2c3f306736c564244d1087
fda77 <fda77@users.noreply.github.com>
2024-02-05 16:58:53 +0100

    sed-host: use variables

--- make/host-tools/sed-host/sed-host.mk
+++ make/host-tools/sed-host/sed-host.mk
@@ -3,6 +3,9 @@ $(PKG)_SOURCE:=sed-$($(PKG)_VERSION).tar.xz
 $(PKG)_HASH:=6e226b732e1cd739464ad6862bd1a1aba42d7982922da7a53519631d24975181
 $(PKG)_SITE:=@GNU/sed
 
+$(PKG)_BINARY:=$($(PKG)_DIR)/sed/sed
+$(PKG)_TARGET_BINARY:=$(TOOLS_DIR)/sed
+
 $(PKG)_CONFIGURE_OPTIONS += --prefix=/usr
 $(PKG)_CONFIGURE_OPTIONS += --without-selinux
 $(PKG)_CONFIGURE_OPTIONS += --disable-acl
@@ -12,14 +15,14 @@ $(TOOLS_SOURCE_DOWNLOAD)
 $(TOOLS_UNPACKED)
 $(TOOLS_CONFIGURED_CONFIGURE)
 
-$($(PKG)_DIR)/sed/sed: $($(PKG)_DIR)/.configured
+$($(PKG)_BINARY): $($(PKG)_DIR)/.configured
 	$(TOOLS_SUBMAKE) -C $(SED_HOST_DIR) all
 	touch -c $@
 
-$(TOOLS_DIR)/sed: $($(PKG)_DIR)/sed/sed
+$($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_FILE)
 
-$(pkg)-precompiled: $(TOOLS_DIR)/sed
+$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 
 
 $(pkg)-clean:
@@ -29,6 +32,6 @@ $(pkg)-dirclean:
 	$(RM) -r $(SED_HOST_DIR)
 
 $(pkg)-distclean: $(pkg)-dirclean
-	$(RM) $(TOOLS_DIR)/sed
+	$(RM) $(SED_HOST_TARGET_BINARY)
 
 $(TOOLS_FINISH)
