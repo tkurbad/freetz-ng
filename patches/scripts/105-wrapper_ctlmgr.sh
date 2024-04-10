fa5d7378d1badbfe097bf42725635d00c41cc603
fda77 <fda77@users.noreply.github.com>
2024-04-10 12:06:47 +0200

    wrapper ctlmgr: fix lib path, remove useless sv changes

--- patches/scripts/105-wrapper_ctlmgr.sh
+++ patches/scripts/105-wrapper_ctlmgr.sh
@@ -1,25 +1,6 @@
 [ "$FREETZ_AVM_HAS_AVMCTLMGR_PRELOAD" == "y" ] || return 0
 echo1 "preparing ctlmgr wrapper"
 
-create_LD_PRELOAD_wrapper /usr/bin/ctlmgr libctlmgr.so
-
-if [ -f "${FILESYSTEM_MOD_DIR}/lib/systemd/system/ctlmgr.service" ] ; then
-	echo1 "preparing ctlmgr service"
-
-	ENV_FILE="/etc/.environment.ctlmgr"
-	# create env file
-	echo \
-	  'export LD_PRELOAD="libctlmgr.so${LD_PRELOAD:+:}${LD_PRELOAD}"' > \
-	  "${FILESYSTEM_MOD_DIR}/${ENV_FILE}"
-	# use own env file
-	modsed -r \
-	  "s/^(EnvironmentFile)=.*/\1=${ENV_FILE//\//\\/}/" \
-	  "${FILESYSTEM_MOD_DIR}/lib/systemd/system/ctlmgr.service" \
-	  "=${ENV_FILE}$"
-	# dont use wrapper
-	modsed \
-	  's,^ExecStart=.*/,&avm/,' \
-	  "${FILESYSTEM_MOD_DIR}/lib/systemd/system/ctlmgr.service" \
-	  "/avm/ctlmgr$"
-fi
+# Since FOS 7.5x /lib/ in not into the search path of ctlmgr
+create_LD_PRELOAD_wrapper /usr/bin/ctlmgr /lib/libctlmgr.so
 
