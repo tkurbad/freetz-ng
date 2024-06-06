2ce67494b512de486a95fb7ab5bdf2187fcb7450
fda77 <fda77@users.noreply.github.com>
2024-06-06 22:42:45 +0200

    fix remove-wireguard

--- patches/scripts/510-remove_wireguard.sh
+++ patches/scripts/510-remove_wireguard.sh
@@ -1,20 +1,17 @@
 [ "$FREETZ_REMOVE_AVM_WIREGUARD" == "y" ] || return 0
 echo1 "removing WireGuard files"
 
-for files in \
-  bin/vpnd \
-  bin/wg \
-  bin/wg-addmaster \
-  bin/wg-addslave \
-  bin/wg-init \
-  bin/wg-removepeer \
-  bin/wg-utils \
-  lib/libwireguard.so \
-  ${MODULES_DIR}/extra/wireguard.ko \
-  ${MODULES_DIR}/kernel/drivers/net/wireguard/wireguard.ko \
-  ; do
-	rm_files "${FILESYSTEM_MOD_DIR}/$files"
-done
+rm_files \
+  "${MODULES_DIR}/extra/wireguard.ko" \
+  "${MODULES_DIR}/kernel/drivers/net/wireguard/wireguard.ko" \
+  "${FILESYSTEM_MOD_DIR}bin/vpnd" \
+  "${FILESYSTEM_MOD_DIR}bin/wg" \
+  "${FILESYSTEM_MOD_DIR}bin/wg-addmaster" \
+  "${FILESYSTEM_MOD_DIR}bin/wg-addslave" \
+  "${FILESYSTEM_MOD_DIR}bin/wg-init" \
+  "${FILESYSTEM_MOD_DIR}bin/wg-removepeer" \
+  "${FILESYSTEM_MOD_DIR}bin/wg-utils" \
+  "${FILESYSTEM_MOD_DIR}lib/libwireguard.so"
 
 supervisor_delete_service "vpnd"
 
