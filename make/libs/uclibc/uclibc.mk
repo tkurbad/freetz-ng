33a77db86fb99b28494375c0d134bb9750eb18c5
fda77 <fda77@users.noreply.github.com>
2024-06-17 12:53:14 +0200

    bump uclibc

--- make/libs/uclibc/uclibc.mk
+++ make/libs/uclibc/uclibc.mk
@@ -2,5 +2,5 @@
 ### MANPAGE:=https://uclibc-ng.org/docs/
 ### CHANGES:=https://gogs.waldemar-brodkorb.de/oss/uclibc-ng/releases
 ### CVSREPO:=https://cgit.uclibc-ng.org/cgi/cgit/uclibc-ng.git/
-### VERSION:=0.9.28/0.9.29/0.9.32.1/0.9.33.2/1.0.14/1.0.44
+### VERSION:=0.9.28/0.9.29/0.9.32.1/0.9.33.2/1.0.14/1.0.49
 
