517185e95d28398cb435f089ba9f0b7b85e4c6ac
fda77 <fda77@users.noreply.github.com>
2024-07-06 16:25:55 +0200

    bump ca-bundle

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -52,7 +52,7 @@ Latest changes
    * gcc 3.4.6/4.6.4/4.7.4/4.8.5/5.5.0/8.3.0//8.4.0/9.3.0/13.3.0/14.1.0
 
  - Host tools:
-   * ca-bundle 2024-03-11
+   * ca-bundle 2024-07-02
    * cmake 3.30.0
    * cpio 2.15
    * fakeroot 1.35
--- make/pkgs/ca-bundle/ca-bundle.mk
+++ make/pkgs/ca-bundle/ca-bundle.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 2024-03-11)
+$(call PKG_INIT_BIN, 2024-07-02)
 $(PKG)_SOURCE:=cacert-$($(PKG)_VERSION).pem
-$(PKG)_HASH:=1794c1d4f7055b7d02c2170337b61b48a2ef6c90d77e95444fd2596f4cac609f
+$(PKG)_HASH:=1bf458412568e134a4514f5e170a328d11091e071c7110955c9884ed87972ac9
 $(PKG)_SITE:=https://www.curl.se/ca,https://curl.haxx.se/ca
 ### WEBSITE:=https://www.curl.se/ca
 ### SUPPORT:=fda77
