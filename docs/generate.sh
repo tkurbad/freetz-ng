ae99ad16e5167b74475a4f0d43a4d275dd3e50a1
fda77 <fda77@users.noreply.github.com>
2024-07-12 13:54:22 +0200

    docs: add host-tools

--- docs/generate.sh
+++ docs/generate.sh
@@ -17,6 +17,7 @@ generate_info $@ 2>&1 | grep -v '^nohelp'
 generate_warn "Packages"       "make/generate.sh"
 generate_warn "Libraries"      "libs/generate.sh"
 generate_warn "Patches"        "patches/generate.sh"
+generate_info "Host-Tools"     "host-tools/generate.sh"
 generate_info "Wiki"           "wiki/generate.sh"
 generate_info "Screenshots"    "screenshots/generate.sh"
 generate_info "Prerequisites"  "PREREQUISITES/generate.sh"
