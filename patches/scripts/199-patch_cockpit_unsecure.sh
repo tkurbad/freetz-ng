1f7c41cfbb08b2c89183f0b7084dcc75ffc0b6ce
fda77 <fda77@users.noreply.github.com>
2024-10-23 13:33:27 +0200

    fix typo

--- patches/scripts/199-patch_cockpit_unsecure.sh
+++ patches/scripts/199-patch_cockpit_unsecure.sh
@@ -1,5 +1,5 @@
 [ "$FREETZ_PATCH_COCKPIT_UNSECURE" == "y" ] || return 0
-echo1 "applying cock pit unsecure settings patch"
+echo1 "applying cockpit unsecure settings patch"
 
 cockpit_pruner "osInformations"
 
