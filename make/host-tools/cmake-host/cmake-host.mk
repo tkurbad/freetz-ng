49fb77ed51cc8de040de17e71323094f675efc7b
fda77 <fda77@users.noreply.github.com>
2025-01-02 02:39:43 +0100

    cmake-host: use openssl-host

--- make/host-tools/cmake-host/cmake-host.mk
+++ make/host-tools/cmake-host/cmake-host.mk
@@ -9,13 +9,14 @@ $(PKG)_SITE:=https://github.com/Kitware/CMake/releases/download/v$($(PKG)_VERSIO
 ### CVSREPO:=https://gitlab.kitware.com/cmake/cmake
 ### SUPPORT:=fda77
 
+$(PKG)_DEPENDS_ON+=patchelf-host
+$(PKG)_DEPENDS_ON+=openssl-host
 $(PKG)_DEPENDS_ON+=ca-bundle-host
 
 $(PKG)_DESTDIR             := $(FREETZ_BASE_DIR)/$(TOOLS_BUILD_DIR)
 
 $(PKG)_BINARIES            := ccmake cmake cpack ctest
 $(PKG)_BINARIES_TARGET_DIR := $($(PKG)_BINARIES:%=$($(PKG)_DESTDIR)/bin/%)
-$(PKG)_SHARE_TARGET_DIR    := $($(PKG)_DESTDIR)/share/$(pkg_short)-$($(PKG)_MAJOR_VERSION)
 $(PKG)_DOC_TARGET_DIR      := $($(PKG)_DESTDIR)/doc/$(pkg_short)-$($(PKG)_MAJOR_VERSION)
 
 
@@ -26,8 +27,10 @@ $(PKG)_CONFIGURE_OPTIONS += --no-qt-gui
 $(PKG)_CONFIGURE_OPTIONS += --no-debugger
 $(PKG)_CONFIGURE_OPTIONS += --no-system-libs
 $(PKG)_CONFIGURE_OPTIONS += --
-$(PKG)_CONFIGURE_OPTIONS += -DCMAKE_USE_OPENSSL=OFF
-#$(PKG)_CONFIGURE_OPTIONS += -DOPENSSL_USE_STATIC_LIBS=TRUE
+$(PKG)_CONFIGURE_OPTIONS += -DCMAKE_USE_OPENSSL=ON
+
+$(PKG)_CONFIGURE_OPTIONS += -DOPENSSL_ROOT_DIR="$(OPENSSL_HOST_INSTALLDIR)"
+$(PKG)_CONFIGURE_OPTIONS += -DOPENSSL_INCLUDE_DIR="$(OPENSSL_HOST_INSTALLDIR)/include"
 
 
 $(TOOLS_SOURCE_DOWNLOAD)
@@ -41,15 +44,27 @@ $($(PKG)_DIR)/.compiled: $($(PKG)_DIR)/.configured
 $($(PKG)_DIR)/.installed: $($(PKG)_DIR)/.compiled
 	$(TOOLS_SUBMAKE) -C $(CMAKE_HOST_DIR) install
 	@$(RM) -r "$(CMAKE_HOST_DOC_TARGET_DIR)"
-	@rmdir "$(dir $(CMAKE_HOST_DOC_TARGET_DIR))" || true
+	-@rmdir "$(dir $(CMAKE_HOST_DOC_TARGET_DIR))"
+	$(call CMAKE_HOST_FIXHARDCODED)
 	@touch $@
+
+define $(PKG)_FIXHARDCODED
+	@for binfile in $(CMAKE_HOST_BINARIES); do \
+	for libfile in libcrypto libssl; do \
+	$(PATCHELF) --replace-needed $${libfile}.so.3 $(OPENSSL_HOST_DESTDIR)/$${libfile}.so.3 $(CMAKE_HOST_DESTDIR)/bin/$${binfile} ;\
+	done ;\
+	done ;
+endef
+
+$(pkg)-fixhardcoded:
+	$(call CMAKE_HOST_FIXHARDCODED)
 
 $(pkg)-precompiled: $($(PKG)_DIR)/.installed
 
 
 $(pkg)-clean:
 	-$(MAKE) -C $(CMAKE_HOST_DIR) clean
-	-$(RM) $(CMAKE_HOST_DIR)/.{configured,compiled,installed}
+	$(RM) $(CMAKE_HOST_DIR)/.{configured,compiled,installed,fixhardcoded}
 
 $(pkg)-dirclean:
 	$(RM) -r $(CMAKE_HOST_DIR)
@@ -57,6 +72,8 @@ $(pkg)-dirclean:
 $(pkg)-distclean: $(pkg)-dirclean
 	$(RM) -r \
 		$(CMAKE_HOST_BINARIES_TARGET_DIR) \
-		$(CMAKE_HOST_SHARE_TARGET_DIR)
+		$($(PKG)_DESTDIR)/share/aclocal/cmake.m4 \
+		$($(PKG)_DESTDIR)/share/$(pkg_short)-$($(PKG)_MAJOR_VERSION)/ \
+		$(CMAKE_HOST_DOC_TARGET_DIR)/
 
 $(TOOLS_FINISH)
