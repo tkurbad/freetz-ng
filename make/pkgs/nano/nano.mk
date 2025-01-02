ff94125e085bc40cdbe3343ca6a2044a9d1c12ae
fda77 <fda77@users.noreply.github.com>
2025-01-02 02:35:10 +0100

    bump nano

--- make/pkgs/nano/nano.mk
+++ make/pkgs/nano/nano.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 8.2)
+$(call PKG_INIT_BIN, 8.3)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.xz
-$(PKG)_HASH:=d5ad07dd862facae03051c54c6535e54c7ed7407318783fcad1ad2d7076fffeb
+$(PKG)_HASH:=551b717b2e28f7e90f749323686a1b5bbbd84cfa1390604d854a3ca3778f111e
 $(PKG)_SITE:=https://www.nano-editor.org/dist/v8
 ### WEBSITE:=https://www.nano-editor.org/
 ### MANPAGE:=https://www.nano-editor.org/docs.php
